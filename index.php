c21baa3d55f610f3bd0f0a04a91fc320803ef7d9
Arik Ostler <arikostler@gmail.com>
2016-02-18 12:30:33 -0500

    Added Twitter Bootstrap

--- index.php
+++ index.php
@@ -16,13 +16,6 @@
 		<br>
 		Message: <span id="message"></span>
 	</p>
-	<!--<br><br>
-	<div class="Switch Off">
-		<div class="Toggle"></div>
-		<span class="On">ON</span>
-		<span class="Off">OFF</span>
-	</div>
-	-->
 	<table class="table table-striped">
 		<tr>
 			<td>Circuit ONE</td>
@@ -45,5 +38,15 @@
 			</td>				
 		</tr>
 	</table>
+
+
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
 </body>
 </html>
